d47f1b44f747dddc89e4bd08862387cd1e29baf7
Joe Karasek <joseph.karasek@gmail.com>
2016-04-01 09:07:44 -0700

    add functionality write and read questions to firebase DB

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+    return this.store.findAll('question');
+  },
+  actions: {
+    addQuestion() {
+      var newQuestion = this.store.createRecord('question', {
+        author: "Joe MaMa",
+        body: "This really puzzles me a lot, it would be great if someone can figure it out for me!",
+        question: "If 'i' is an imaginary number, how come I can hold my iPhone?"
+      });
+      newQuestion.save();
+    }
+  }
 });
