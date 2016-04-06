17f9e62ed883393fc1fef86835aac0d794ae3f24
Joe Karasek <joseph.karasek@gmail.com>
2016-04-05 19:15:06 -0700

    deploy script now merges

--- scripts/deploy-to-github-pages.sh
+++ scripts/deploy-to-github-pages.sh
@@ -1,11 +1,12 @@
+echo "Switch to gh-pages branch"
+git checkout gh-pages
+git merge master
+
 echo "Clean up"
 rm -rf dist
 
 echo "Create new production build"
 ember build --environment production
-
-echo "Switch to gh-pages branch"
-git checkout gh-pages
 
 echo "Add dist to commit and push it to GitHub"
 git add dist --force
